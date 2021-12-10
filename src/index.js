2be20eb7b1d4993a61e74fb61ef98a88453685eb
Brian Nwaeke <bnwaeke@icloud.com>
2021-12-10 14:40:56 -0800

    update eslint settings

--- src/index.js
+++ src/index.js
@@ -6,28 +6,36 @@ import './index.css';
 
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
     render() {
       return (
         <button 
           className="square" 
-          onClick={() => this.setState({value: 'X'})}
+          onClick={() => this.props.onClick()}
           >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
   }
+
+
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+
     // eslint-disable-next-line no-unused-vars
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+         <Square 
+            value={this.state.squares[i]} 
+            onClick={() => this.handleClick(i)}
+          />
+      );
     }
   
     render() {
@@ -56,6 +64,9 @@ class Square extends React.Component {
     }
   }
   
+
+
+
   class Game extends React.Component {
     render() {
       return (
